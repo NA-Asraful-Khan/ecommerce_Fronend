77cae9ec82e9fba3014ca47f9c68cc3a74a25e1d
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-08-02 08:15:57 +0600

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
 import './App.scss';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// pages
+import {Home, Category, Cart} from "./pages/index";
+// components
+import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer";
+import {Provider} from 'react-redux';
+import store from "./store/store";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Provider store = {store}>
+        <BrowserRouter>
+          <Navbar />
+          
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
